5b79f1a2e862b78a20d97c49d9a7b386bd321d70
George Crawford <georgecrawford@Mac.fios-router.home>
2025-05-07 21:53:48 -0400

    add dihybrid cross functionality, create transitions and final touches

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ CORS(app)
 def index():
   return render_template('index.html')
 
+# monohybrid logic
 def mono_punnett(parent1, parent2):
   parent1gamete1 = parent1[0]
   parent1gamete2 = parent1[1]
@@ -32,8 +33,7 @@ def mono_punnett(parent1, parent2):
 
   return offspring1, offspring2, offspring3, offspring4
 
-
-def genotypic_ratio(offspring1, offspring2, offspring3, offspring4):
+def mono_genotypic_ratio(offspring1, offspring2, offspring3, offspring4):
   offspring_arr = [offspring1, offspring2, offspring3, offspring4]
   homozygous_dom = 0
   heterozygous = 0
@@ -56,8 +56,7 @@ def genotypic_ratio(offspring1, offspring2, offspring3, offspring4):
 
   return geno_ratio
 
-
-def phenotypic_ratio(offspring1, offspring2, offspring3, offspring4):
+def mono_phenotypic_ratio(offspring1, offspring2, offspring3, offspring4):
   offspring_arr = [offspring1, offspring2, offspring3, offspring4]
 
   dominant_traits = 0
@@ -78,8 +77,8 @@ def phenotypic_ratio(offspring1, offspring2, offspring3, offspring4):
   
   return pheno_ratio
 
-@app.route('/calculate', methods=['POST'])
-def calculate_punnett():
+@app.route('/calculatemono', methods=['POST'])
+def calculate_mono_punnett():
     data = request.get_json()
     if not data or 'parent1' not in data or 'parent2' not in data:
         return jsonify({'error': 'Please provide both parent1 and parent2 genotypes'}), 400
@@ -89,16 +88,175 @@ def calculate_punnett():
     
     offspring1, offspring2, offspring3, offspring4 = mono_punnett(parent1, parent2)
     
-    geno_ratio = genotypic_ratio(offspring1, offspring2, offspring3, offspring4)
-    pheno_ratio = phenotypic_ratio(offspring1, offspring2, offspring3, offspring4)
+    geno_ratio = mono_genotypic_ratio(offspring1, offspring2, offspring3, offspring4)
+    pheno_ratio = mono_phenotypic_ratio(offspring1, offspring2, offspring3, offspring4)
     
     response = {
-        'offspring1': offspring1,
-        'offspring2': offspring2,
-        'offspring3': offspring3,
-        'offspring4': offspring4,
-        'geno_ratio': geno_ratio,
-        'pheno_ratio': pheno_ratio
+      'offspring1': offspring1,
+      'offspring2': offspring2,
+      'offspring3': offspring3,
+      'offspring4': offspring4,
+      'geno_ratio': geno_ratio,
+      'pheno_ratio': pheno_ratio
+    }
+    
+    return jsonify(response), 200
+
+
+# dihybrid logic
+def di_punnett(parent1, parent2):
+  parent1gamete1 = parent1[0] + parent1[2]
+  parent1gamete2 = parent1[0] + parent1[3]
+  parent1gamete3 = parent1[1] + parent1[2]
+  parent1gamete4 = parent1[1] + parent1[3]
+  parent2gamete1 = parent2[0] + parent2[2]
+  parent2gamete2 = parent2[0] + parent2[3]
+  parent2gamete3 = parent2[1] + parent2[2]
+  parent2gamete4 = parent2[1] + parent2[3]
+
+  def combine_alleles(gameteX, gameteY):
+    if gameteX[0].isupper():
+      offspring_gene1 = gameteX[0] + gameteY[0]
+    else:
+      offspring_gene1 = gameteY[0] + gameteX[0]
+
+    if gameteX[1].isupper():
+      offspring_gene2 = gameteX[1] + gameteY[1]
+    else:
+      offspring_gene2 = gameteY[1] + gameteX[1]
+
+    offspring = offspring_gene1 + offspring_gene2
+    return offspring
+
+  offspring_arr = []
+
+  parent1_gametes_arr = [parent1gamete1, parent1gamete2, parent1gamete3, parent1gamete4]
+  parent2_gametes_arr = [parent2gamete1, parent2gamete2, parent2gamete3, parent2gamete4]
+
+  for parent2_gamete in parent2_gametes_arr:
+    for parent1_gamete in parent1_gametes_arr:
+      offspring = combine_alleles(parent1_gamete, parent2_gamete)
+      offspring_arr.append(offspring)
+  
+  return offspring_arr
+
+from math import gcd
+from functools import reduce
+
+def simplify_ratio(*numbers):
+  GCD = reduce(gcd, numbers)
+
+  simplified_values = []
+  for n in numbers:
+    simplified = n // GCD
+    simplified_values.append(simplified)
+
+  return simplified_values
+
+def di_genotypic_ratio(offspring_arr):
+  homo_dom1_2 = 0
+  homo_dom1_hetero2 = 0
+  homo_dom1_homo_rec2 = 0
+
+  hetero1_homo_dom2 = 0
+  hetero1_2 = 0
+  hetero1_homo_rec2 = 0
+
+  homo_rec1_homo_dom2 = 0
+  homo_rec1_hetero2 = 0
+  homo_rec1_2 = 0
+
+  for genotype in offspring_arr:
+    if genotype[:2].isupper() and genotype[-2:].isupper():
+      homo_dom1_2 += 1
+    elif genotype[:2].isupper() and genotype[2].isupper() and genotype[3].islower():
+      homo_dom1_hetero2 += 1
+    elif genotype[:2].isupper() and genotype[-2:].islower():
+      homo_dom1_homo_rec2 += 1
+    elif genotype[0].isupper() and genotype[1].islower() and genotype[2].isupper() and genotype[3].islower():
+      hetero1_2 += 1
+    elif genotype[0].isupper() and genotype[1].islower() and genotype[-2:].isupper():
+      hetero1_homo_dom2 += 1
+    elif genotype[0].isupper() and genotype[1].islower() and genotype[-2:].islower():
+      hetero1_homo_rec2 += 1
+    elif genotype[:2].islower() and genotype[-2:].isupper():
+      homo_rec1_homo_dom2 += 1
+    elif genotype[:2].islower() and genotype[2].isupper() and genotype[3].islower():
+      homo_rec1_hetero2 += 1
+    else:
+      homo_rec1_2 += 1
+
+  geno_arr = [
+    homo_dom1_2,
+    homo_dom1_hetero2,
+    homo_dom1_homo_rec2,
+    hetero1_homo_dom2,
+    hetero1_2,
+    hetero1_homo_rec2,
+    homo_rec1_homo_dom2,
+    homo_rec1_hetero2,
+    homo_rec1_2
+  ]
+
+  simplified_geno_arr = simplify_ratio(*geno_arr) 
+
+  geno_ratio = ""
+  for i in simplified_geno_arr:
+    if i >= 1:
+      geno_ratio += f" {i} :"
+  geno_ratio = geno_ratio[1:]
+  geno_ratio = geno_ratio[:-1]
+
+  return geno_ratio
+
+
+def di_phenotypic_ratio(offspring_arr):
+  dom_traits1_2 = 0
+  dom_trait1_rec_trait2 = 0
+  rec_trait1_dom_trait2 = 0
+  rec_traits1_2 = 0
+
+  for genotype in offspring_arr:
+    if genotype[0].isupper() and genotype[2].isupper():
+      dom_traits1_2 += 1
+    elif genotype[0].isupper() and genotype[2].islower():
+      dom_trait1_rec_trait2 += 1
+    elif genotype[0].islower() and genotype[2].isupper():
+      rec_trait1_dom_trait2 += 1
+    elif genotype[0].islower() and genotype[2].islower():
+      rec_traits1_2 += 1
+
+  pheno_arr = [dom_traits1_2, dom_trait1_rec_trait2, rec_trait1_dom_trait2, rec_traits1_2]
+  simplified_pheno_arr = simplify_ratio(*pheno_arr)
+
+  pheno_ratio = ""
+  for i in simplified_pheno_arr:
+    if i >= 1:
+      pheno_ratio += f" {i} :"
+  pheno_ratio = pheno_ratio[1:]
+  pheno_ratio = pheno_ratio[:-1]
+  
+  return pheno_ratio
+
+
+
+@app.route('/calculateddi', methods=['POST'])
+def calculate_di_punnett():
+    data = request.get_json()
+    if not data or 'parent1' not in data or 'parent2' not in data:
+        return jsonify({'error': 'Please provide both parent1 and parent2 genotypes'}), 400
+    
+    parent1 = data['parent1']
+    parent2 = data['parent2']
+
+    offspring_arr = di_punnett(parent1, parent2)
+    geno_ratio = di_genotypic_ratio(offspring_arr)
+    pheno_ratio = di_phenotypic_ratio(offspring_arr)
+
+    response = {
+      'offspring': offspring_arr,
+      'geno_ratio': geno_ratio,
+      'pheno_ratio': pheno_ratio
     }
     
     return jsonify(response), 200
--- static/script.js
+++ static/script.js
@@ -12,35 +12,38 @@ function toggleMonoCross() {
 }
 
 
-const button = document.getElementById('calculate');
+const monoButton = document.getElementById('mono-button');
 
-const parent1GenoDescription = document.querySelector('.parent1-geno');
-const parent2GenoDescription = document.querySelector('.parent2-geno');
+const parent1GenoDescriptionMono = document.getElementById('parent1-geno-mono');
+const parent2GenoDescriptionMono = document.getElementById('parent2-geno-mono');
 
-const parent1Gamete1 = document.getElementById('parent1-gamete1');
-const parent1Gamete2 = document.getElementById('parent1-gamete2');
-const parent2Gamete1 = document.getElementById('parent2-gamete1');
-const parent2Gamete2 = document.getElementById('parent2-gamete2');
+const parent1Gamete1Mono = document.getElementById('parent1-gamete1-mono');
+const parent1Gamete2Mono = document.getElementById('parent1-gamete2-mono');
+const parent2Gamete1Mono = document.getElementById('parent2-gamete1-mono');
+const parent2Gamete2Mono = document.getElementById('parent2-gamete2-mono');
 
 const offspring1Mono = document.getElementById('offspring1-mono');
 const offspring2Mono = document.getElementById('offspring2-mono');
 const offspring3Mono = document.getElementById('offspring3-mono');
 const offspring4Mono = document.getElementById('offspring4-mono');
 
-button.addEventListener('click', function() {
-    const parent1Input = document.getElementById('parent1-mono').value;
-    const parent2Input = document.getElementById('parent2-mono').value;
+const genotypicRatioMono = document.getElementById('geno-ratio-mono');
+const phenotypicRatioMono = document.getElementById('pheno-ratio-mono');
 
-    parent1Gamete1.innerText = parent1Input.charAt(0);
-    parent1Gamete2.innerText = parent1Input.charAt(1);
-    parent2Gamete1.innerText = parent2Input.charAt(0);
-    parent2Gamete2.innerText = parent2Input.charAt(1);
+monoButton.addEventListener('click', function() {
+    const parent1Input = document.getElementById('parent1-input-mono').value;
+    const parent2Input = document.getElementById('parent2-input-mono').value;
 
-    document.querySelector('caption').style.visibility = 'visible';
-    parent1GenoDescription.innerText = parent1Input;
-    parent2GenoDescription.innerText = parent2Input;
+    parent1Gamete1Mono.innerText = parent1Input.charAt(0);
+    parent1Gamete2Mono.innerText = parent1Input.charAt(1);
+    parent2Gamete1Mono.innerText = parent2Input.charAt(0);
+    parent2Gamete2Mono.innerText = parent2Input.charAt(1);
 
-    fetch('/calculate', {
+    document.getElementById('mono-caption').style.visibility = 'visible';
+    parent1GenoDescriptionMono.innerText = parent1Input;
+    parent2GenoDescriptionMono.innerText = parent2Input;
+
+    fetch('/calculatemono', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -63,15 +66,90 @@ button.addEventListener('click', function() {
         offspring2Mono.innerText = data.offspring2;
         offspring3Mono.innerText = data.offspring3;
         offspring4Mono.innerText = data.offspring4;
+
+        const offspringArrMono = [offspring1Mono, offspring2Mono, offspring3Mono, offspring4Mono];
+        const delay = 50;
+        offspringArrMono.forEach((offspring, index) => {
+            setTimeout(() => {
+                offspring.classList.add('fade-in');
+            }, delay * index);
+        });
+
+        genotypicRatioMono.innerText = data.geno_ratio;
+        phenotypicRatioMono.innerText = data.pheno_ratio;
     })
     .catch(error => {
         console.error('Error:', error);
     });
 });
 
-/*
-These parameters are determined by the fetch API's promise chain:
-The first .then() receives the Response object from the fetch promise.
-The second .then() receives the result of the response.json() promise.
-The .catch() block receives any error that occurs during the fetch request or subsequent promise resolution.
-*/
+
+const diButton = document.getElementById('di-button');
+
+const parent1GenoDescriptionDi = document.getElementById('parent1-geno-di');
+const parent2GenoDescriptionDi = document.getElementById('parent2-geno-di');
+
+const parent1Gamete1Di = document.getElementById('parent1-gamete1-di');
+const parent1Gamete2Di = document.getElementById('parent1-gamete2-di');
+const parent1Gamete3Di = document.getElementById('parent1-gamete3-di');
+const parent1Gamete4Di = document.getElementById('parent1-gamete4-di');
+const parent2Gamete1Di = document.getElementById('parent2-gamete1-di');
+const parent2Gamete2Di = document.getElementById('parent2-gamete2-di');
+const parent2Gamete3Di = document.getElementById('parent2-gamete3-di');
+const parent2Gamete4Di = document.getElementById('parent2-gamete4-di');
+
+const genotypicRatioDi = document.getElementById('geno-ratio-di');
+const phenotypicRatioDi = document.getElementById('pheno-ratio-di');
+
+diButton.addEventListener('click', function() {
+    const parent1Input = document.getElementById('parent1-input-di').value;
+    const parent2Input = document.getElementById('parent2-input-di').value;
+
+    parent1Gamete1Di.innerText = parent1Input.charAt(0) + parent1Input.charAt(2);
+    parent1Gamete2Di.innerText = parent1Input.charAt(0) + parent1Input.charAt(3);
+    parent1Gamete3Di.innerText = parent1Input.charAt(1) + parent1Input.charAt(2);
+    parent1Gamete4Di.innerText = parent1Input.charAt(1) + parent1Input.charAt(3);
+    parent2Gamete1Di.innerText = parent2Input.charAt(0) + parent2Input.charAt(2);
+    parent2Gamete2Di.innerText = parent2Input.charAt(0) + parent2Input.charAt(3);
+    parent2Gamete3Di.innerText = parent2Input.charAt(1) + parent2Input.charAt(2);
+    parent2Gamete4Di.innerText = parent2Input.charAt(1) + parent2Input.charAt(3);
+
+    document.getElementById('di-caption').style.visibility = 'visible';
+    parent1GenoDescriptionDi.innerText = parent1Input;
+    parent2GenoDescriptionDi.innerText = parent2Input;
+
+    fetch('/calculateddi', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            parent1: parent1Input,
+            parent2: parent2Input
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response from server:', data);
+
+        const diPunnettCells = document.querySelectorAll('.di-punnett td');
+        const delay = 50;
+        data.offspring.forEach((offspring, index) => {
+            setTimeout(() => {
+                diPunnettCells[index].innerText = offspring;
+                diPunnettCells[index].classList.add('fade-in');
+            }, delay * index);
+        });
+        
+        genotypicRatioDi.innerText = data.geno_ratio;
+        phenotypicRatioDi.innerText = data.pheno_ratio;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
